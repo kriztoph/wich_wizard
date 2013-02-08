56f98a7534cbc415bfda122b4ba1072aac9461c9
Kriztopher Mendoza <kriz@krizinc.com>
2013-02-07 21:12:28 -0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WichWizard::Application.config.secret_token = 'bc58424b0601b60dd008a8d76ba71d223935657ac385a8c41e662c7f8fa27e130f4c9f0a3b6807f568b9e4cbb2ca7a2b9153ff64b42d6e87858dc6b259e0f44a'
