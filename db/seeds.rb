a8f0059a39ac639b4c96e926d12d4979ad81e9b6
Kriztopher Mendoza <kriz@krizinc.com>
2013-02-08 18:08:15 -0800

    add cheese and more condiments to seed, change copyright date, add bread and cheeses to order show page, align quote

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,12 +18,20 @@ SandwichIngredient.create([{:category => 'Meat', :ingredient => 'Turkey'},
                           {:category => 'Veggie', :ingredient => 'Onion'},
                           {:category => 'Veggie', :ingredient => 'Avocado'},
 
+                          {:category => 'Cheese', :ingredient => 'Provolone'},
+                          {:category => 'Cheese', :ingredient => 'Cheddar'},
+                          {:category => 'Cheese', :ingredient => 'Pepperjack'},
+                          {:category => 'Cheese', :ingredient => 'Swiss'},
+                          {:category => 'Cheese', :ingredient => 'American'},
+
                           {:category => 'Bread', :ingredient => 'White'},
                           {:category => 'Bread', :ingredient => 'Wheat'},
                           {:category => 'Bread', :ingredient => 'Sourdough'},
                           {:category => 'Bread', :ingredient => 'Dutch Crunch'},
 
                           {:category => 'Condiment', :ingredient => 'Mayo'},
+                          {:category => 'Condiment', :ingredient => 'BBQ Sauce'},
+                          {:category => 'Condiment', :ingredient => 'Pickles'},
                           {:category => 'Condiment', :ingredient => 'Ketchup'},
                           {:category => 'Condiment', :ingredient => 'Mustard'},
                           {:category => 'Condiment', :ingredient => 'Ranch'}])
